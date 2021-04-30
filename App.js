62bb3747141b9a68005c8eede6a672354a9ddcaf
Camisw <alvesbrandaocamila@gmail.com>
2021-04-29 21:55:41 -0300

    Atividade 2 - PAM-1

--- App.js
+++ App.js
@@ -1,21 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import axios from 'axios'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+export default class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      endereco : ''
+    }
+  }
+
+  componentDidMount(){
+    axios
+    .get('https://viacep.com.br/ws/06401135/json')
+    .then(response => {
+
+      console.log(response.data)
+
+      this.setState({
+        endereco : response.data
+      })
+    })
+  };
+
+  render(){
+    return <View style={ styles.container }>
+      <Text>CEP: {this.state.endereco.cep}</Text>
+      <Text>Complemento: {this.state.endereco.complemento}</Text>
+      <Text>Bairro: {this.state.endereco.bairro}</Text>
+      <Text>Localidade: {this.state.endereco.localidade}</Text>
+      <Text>UF: {this.state.endereco.uf}</Text>
+      <Text>Logradouro: {this.state.endereco.logradouro}</Text>
+      <Text>IBGE: {this.state.endereco.ibge}</Text>
+      <Text>GIA: {this.state.endereco.gia}</Text>
+      <Text>DDD: {this.state.endereco.ddd}</Text>
+      <Text>SIAFI: {this.state.endereco.siafi}</Text>
     </View>
-  );
-}
+  }
+
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffe',
     alignItems: 'center',
     justifyContent: 'center',
+
   },
 });
+
